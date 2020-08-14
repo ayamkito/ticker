cd167c1c3b802c6c31b6e40c6e9aaad11babe9e9
akwotiga kizito <kakwotiga@yahoo.com>
2020-08-13 20:56:14 -0400

    css not complete, pause function not complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ticker extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      pause: true
+    }
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }, 1000)
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    if(this.state.pause){
+      if(nextState.count % 3===0){
+        return true
+      }
+    }
+    else return false;
+
+  }
+  reset=()=>{
+    this.setState({
+      count: 0
+    })
+  }
+  pause=()=>{
+    if(this.state.pause){
+      this.setState({
+        pause:false
+      })
+    }else{
+      this.setState({
+        pause:true
+      })
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <p>Count:{this.state.count}</p>
+        <button type="button" onClick ={this.reset} >Reset</button>
+        <button type="button" onClick={this.pause}>{this.state.pause? "Pause Ticker":"Ticker is paused" }</button>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default ticker
